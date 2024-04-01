57b7c50fc0d03bfedb49a80c2c20d4a0a59f01c2
Никита Фомин <defdXs@yandex.ru>
2024-04-01 14:06:04 +0300

    aaaaaaaaa

--- OperationsResearch/MainWindow.xaml.cs
+++ OperationsResearch/MainWindow.xaml.cs
@@ -1,13 +1,40 @@
 using Microsoft.UI.Xaml;
 using OperationsResearch.Services;
+using OperationsResearch.Structures;
+using System;
+using System.Collections.Generic;
+using System.Data;
 
 namespace OperationsResearch;
 public sealed partial class MainWindow : Window
 {
+    Table Values = new();
+
     public MainWindow()
     {
         this.InitializeComponent();
 
         LogService.Log("Application launched");
+
+        FillInitialTable();
+    }
+
+    private void FillInitialTable()
+    {
+        Values.Rows.Add(new List<object>(){ "1", "4", "5", "3", "1" });
+        Values.Rows.Add(new List<object>() { "2", "3", "1", "4", "2" });
+        Values.Rows.Add(new List<object>() { "2", "1", "3", "1", "2" });
+        Values.Headers = new List<string>() { "B1=100", "B2=100", "B3=80", "B4=90", "B5=70" };
+        Values.Names = new List<string>() { "A1=200", "A2=350", "A3=150" };
+    }
+
+    private void ShowInitialTableButton_Click(object sender, RoutedEventArgs e)
+    {
+        LogService.Log(Values.ToLongString());
+    }
+
+    private void ShowLabInfo_Click(object sender, RoutedEventArgs e)
+    {
+
     }
 }
---
+++ OperationsResearch/Structures/Table.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationsResearch.Structures;
+
+internal class Table
+{
+    public List<string> Headers;
+    public List<string> Names;
+    public List<List<object>> Rows;
+    
+    public Table()
+    {
+        Headers = new();
+        Names = new();
+        Rows = new();
+    }
+
+    public string ToLongString()
+    {
+        var output = "";
+
+        var hasHeader = Headers.Count > 0;
+        var hasHeaderColumn = Names.Count > 0;
+
+        if (hasHeader)
+        {
+            if (hasHeaderColumn) output += "\t";
+            foreach (var header in Headers) { output += header + "\t"; }
+            output += "\n";
+        }
+
+        var rowCounter = 0;
+        foreach (var row in Rows)
+        {
+            if (rowCounter > 0) output += "\n";
+            if (hasHeaderColumn) { output += Names[rowCounter] + "\t"; }
+            foreach (var value in row) { output += value + "\t"; }
+            rowCounter++;
+        }
+
+        return output;
+    }
+}
