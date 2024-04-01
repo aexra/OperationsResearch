457216703e9200924f40ae649c34eb4f01d8e1ad
Никита Фомин <defdXs@yandex.ru>
2024-04-01 15:12:54 +0300

    mine

--- OperationsResearch/MainWindow.xaml.cs
+++ OperationsResearch/MainWindow.xaml.cs
@@ -28,14 +28,16 @@ public sealed partial class MainWindow : Window
     // WEIRD STUFF FOR LAB
     private void FillInitialTable()
     {
-        //Values.Rows.Add(new List<object>(){ 1, 4, 5, 3, 1 });
-        //Values.Rows.Add(new List<object>() { 2, 3, 1, 4, 2 });
-        //Values.Rows.Add(new List<object>() { 2, 1, 3, 1, 2 });
-        //Values.Headers = new List<string>() { "B1=100", "B2=100", "B3=80", "B4=90", "B5=70"};
-        //Values.Names = new List<string>() { "A1=200", "A2=350", "A3=150" };
-        InitialValues.Rows.Add(new List<object>() { 10, 1, 3 });
-        InitialValues.Rows.Add(new List<object>() { 10, 2, 1 });
-        InitialValues.Rows.Add(new List<object>() { 50, 100 });
+        // Мой вариант
+        InitialValues.Rows.Add(new List<object>() { 200, 1, 4, 5, 3, 1 });
+        InitialValues.Rows.Add(new List<object>() { 350, 2, 3, 1, 4, 2 });
+        InitialValues.Rows.Add(new List<object>() { 150, 2, 1, 3, 1, 2 });
+        InitialValues.Rows.Add(new List<object>() { 100, 100, 80, 90, 70 });
+
+        // Пример с capacity < requests
+        //InitialValues.Rows.Add(new List<object>() { 10, 1, 3 });
+        //InitialValues.Rows.Add(new List<object>() { 10, 2, 1 });
+        //InitialValues.Rows.Add(new List<object>() { 50, 100 });
     }
     private List<int> GetCapacity()
     {
