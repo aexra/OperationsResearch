f9b11f2b0bae7c9fef601fdc80aeef36c4be4865
Никита Фомин <defdXs@yandex.ru>
2024-04-03 22:34:09 +0300

    больше костылей!!!

--- OperationsResearch/Classes/TransportProblem/Plan.cs
+++ OperationsResearch/Classes/TransportProblem/Plan.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 namespace OperationsResearch.Classes.TransportProblem;
 public class Plan
 {
+    public int[][] Values;
     public object[][] Mask;
     public List<Vector4> Path;
     public TransportProblem Problem;
@@ -17,6 +18,26 @@ public class Plan
         Problem = problem;
         Mask = mask;
         Path = path;
+
+        Values = new int[mask.Length][];
+        for (var y = 0; y < mask.Length; y++)
+        {
+            Values[y] = new int[mask[y].Length];
+        }
+        for (var y = 0; y < mask.Length; y++)
+        {
+            for (var x = 0; x < mask[y].Length; x++)
+            {
+                if (mask[y][x] is char)
+                {
+                    Values[y][x] = (int)path.Find(v => v.X == x && v.Y == y).W;
+                }
+                else
+                {
+                    Values[y][x] = (int)mask[y][x];
+                }
+            }
+        }
     }
 
     public int BasisCount()
@@ -124,13 +145,78 @@ public class Plan
         }
 
         // Начинаем цикл из нее
-        List<Vector3> negative = new();
         List<Vector2> path = new();
         path.Add(new(minDelta.X, minDelta.Y));
         var closed = false;
+        bool? dir = null;
         while (!closed)
         {
+            // Впервые в цикле (ищем первый отрезок)
+            if (dir == null)
+            {
+                // Пробуем найти по вертикали
+                for (var y = 0; y < deltas.Length; y++)
+                {
+                    if (deltas[y][(int)path.First().X] is char)
+                    {
+                        path.Add(new(y, (int)path.First().X));
+                        dir = true;
+                    }
+                }
 
+                // Пробуем найти по горизонтали
+                for (var x = 0; x < deltas[0].Length; x++)
+                {
+                    if (deltas[(int)path.First().Y][x] is char)
+                    {
+                        path.Add(new(x, (int)path.First().Y));
+                        dir = false;
+                    }
+                }
+            }
+            // Нужна горизонталь
+            else if (dir.Value)
+            {
+                for (var x = 0; x < deltas[0].Length; x++)
+                {
+                    var y = (int)path.Last().Y;
+                    if (deltas[y][x] is char)
+                    {
+                        if (path.Exists(v => v.X == x && v.Y == y)) continue;
+                        path.Add(new(x, y));
+                        dir = false;
+                    }
+                }
+            }
+            // Нужна вертикаль
+            else
+            {
+                for (var y = 0; y < deltas.Length; y++)
+                {
+                    var x = (int)path.Last().X;
+                    if (deltas[y][x] is char)
+                    {
+                        if (path.Exists(v => v.X == x && v.Y == y)) continue;
+                        path.Add(new(x, y));
+                        dir = true;
+                    }
+                }
+            }
+
+            if (path.First().X == path.Last().X && path.First().Y == path.Last().Y) break;
         }
+        
+        // Соберем все негативные перемещения из пути
+        List<Vector3> negative = new();
+        for (var i = 1; i < path.Count; i += 2)
+        {
+            negative.Add(new(path[i].X, path[i].Y, Problem.GetCost((int)path[i].Y, (int)path[i].X)));
+        }
+
+        // Минимальное из них
+        var minCost = negative.Select(x => (int)x.Z).Min();
+
+        // Пройдем по пути и изменим цену значений в маске
+
     }
 }
